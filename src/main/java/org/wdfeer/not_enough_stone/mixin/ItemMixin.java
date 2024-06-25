b04a9721f7a594c9f873b4e5d9f40b76bf1d7bdc
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-25 18:02:03 +0200

    Fix tooltip translation not being loaded

--- src/main/java/org/wdfeer/not_enough_stone/mixin/ItemMixin.java
+++ src/main/java/org/wdfeer/not_enough_stone/mixin/ItemMixin.java
@@ -3,8 +3,8 @@ package org.wdfeer.not_enough_stone.mixin;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,7 +19,7 @@ public class ItemMixin {
     void AppendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci){
         if (stack.hasNbt() && stack.getOrCreateNbt().contains("stones_combined")){
             int stones = stack.getOrCreateNbt().getInt("stones_combined");
-            tooltip.add(Text.translatable("org.wdfeer.not_enough_stone.tooltip").append(Integer.toString(stones)));
+            tooltip.add(Text.translatable("org.wdfeer.not_enough_stone.tooltip").append(Integer.toString(stones)).formatted(Formatting.GRAY));
         }
     }
 }
