248902d07a4202626e8e3bcaa6d7b4b7816421ca
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-26 18:03:27 +0200

    Change Mining Speed based on "stones_combined"

--- src/main/kotlin/org/wdfeer/not_enough_stone/item/GeomatterPickaxe.kt
+++ src/main/kotlin/org/wdfeer/not_enough_stone/item/GeomatterPickaxe.kt
@@ -1,13 +1,17 @@
 package org.wdfeer.not_enough_stone.item
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.block.BlockState
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.ItemStack
 import net.minecraft.item.PickaxeItem
 import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.world.World
 import org.wdfeer.not_enough_stone.material.GeomatterMaterial
 import org.wdfeer.not_enough_stone.tooltip.StonesCombinedTooltip
+import kotlin.math.log
+import kotlin.math.roundToInt
 
 class GeomatterPickaxe : PickaxeItem(GeomatterMaterial.INSTANCE, 3, 1.2f, FabricItemSettings()), Identifiable {
     override fun getIdName(): String = "geomatter_pickaxe"
@@ -18,6 +22,31 @@ class GeomatterPickaxe : PickaxeItem(GeomatterMaterial.INSTANCE, 3, 1.2f, Fabric
         tooltip: MutableList<Text>?,
         context: TooltipContext?
     ) {
-        StonesCombinedTooltip.appendTooltip(stack, tooltip);
+        StonesCombinedTooltip.appendTooltip(stack, tooltip)
+
+        if (tooltip != null){
+            val miningSpeed: Float = getMiningSpeedMultiplier(stack, null)
+            tooltip.add(
+                Text.translatable("not_enough_stone.pickaxe_mining_speed_tooltip")
+                    .formatted(Formatting.GRAY)
+                    .append(Text.of(((miningSpeed * 100f).roundToInt() / 100f).toString())).formatted(getMiningSpeedTooltipColor(miningSpeed)))
+        }
+    }
+
+    private fun getMiningSpeedTooltipColor(miningSpeed: Float): Formatting {
+        if (miningSpeed < 2)
+            return Formatting.GRAY
+        else if (miningSpeed < 3)
+            return Formatting.WHITE
+        else if (miningSpeed < 4)
+            return Formatting.GOLD
+
+        return Formatting.AQUA
+    }
+
+    override fun getMiningSpeedMultiplier(stack: ItemStack?, state: BlockState?): Float {
+        if (stack == null) return 1f
+
+        return log((stack.orCreateNbt.getInt(Geomatter.STONES_COMBINED_NBT) - 26f), 9f) + 1f
     }
 }
