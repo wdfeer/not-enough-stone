ff84fad077e13f9fade2133678387021f9abb8e5
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-06-27 09:56:21 +0200

    Add GeomatterShovel and Hoe (except textures)

---
+++ src/main/kotlin/org/wdfeer/not_enough_stone/item/GeomatterHoe.kt
@@ -0,0 +1,41 @@
+package org.wdfeer.not_enough_stone.item
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.block.BlockState
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.item.HoeItem
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.world.World
+import org.wdfeer.not_enough_stone.material.GeomatterMaterial
+import org.wdfeer.not_enough_stone.tooltip.StonesCombinedTooltip
+
+class GeomatterHoe : HoeItem(GeomatterMaterial.INSTANCE, 1, 1.2f - 4f, FabricItemSettings()), GeomatterMiningTool, Identifiable {
+    override fun getIdName(): String = "geomatter_pickaxe"
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        StonesCombinedTooltip.appendTooltip(stack, tooltip)
+
+        if (tooltip != null){
+            val miningSpeed: Float = getMiningSpeedMultiplier(stack, null)
+            tooltip.add(
+                getMiningSpeedTooltip(miningSpeed)
+            )
+        }
+    }
+
+    override fun getMiningSpeedMultiplier(stack: ItemStack?, state: BlockState?): Float {
+        var multiplier = 1f
+
+        if (stack != null && (state == null || isSuitableFor(state)) && stack.orCreateNbt.contains(Geomatter.STONES_COMBINED_NBT)) {
+            multiplier = getMiningSpeed(stack.orCreateNbt.getInt(Geomatter.STONES_COMBINED_NBT))
+        }
+
+        return (if (state == null) 1f else super.getMiningSpeedMultiplier(stack, state)) * multiplier
+    }
+}
--- src/main/kotlin/org/wdfeer/not_enough_stone/item/ModItems.kt
+++ src/main/kotlin/org/wdfeer/not_enough_stone/item/ModItems.kt
@@ -3,19 +3,17 @@ package org.wdfeer.not_enough_stone.item
 import net.minecraft.item.Item
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
-import net.minecraft.util.Identifier
 
 class ModItems {
     companion object{
         val GEOMATTER = Geomatter()
-        private val GEOMATTER_PICKAXE = GeomatterPickaxe()
-        private val GEOMATTER_AXE = GeomatterAxe()
-        private val GEOMATTER_SWORD = GeomatterSword()
         val items: Array<Item> = arrayOf(
             GEOMATTER,
-            GEOMATTER_PICKAXE,
-            GEOMATTER_AXE,
-            GEOMATTER_SWORD
+            GeomatterPickaxe(),
+            GeomatterAxe(),
+            GeomatterSword(),
+            GeomatterShovel(),
+            GeomatterHoe()
         )
 
         fun initialize() {
