084a517c3eb508577f380cebd66b1447aab2c1e7
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-07 20:43:57 +0200

    Add GeomatterClaymore

--- src/main/kotlin/org/wdfeer/not_enough_stone/item/ModItems.kt
+++ src/main/kotlin/org/wdfeer/not_enough_stone/item/ModItems.kt
@@ -6,28 +6,27 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import org.wdfeer.not_enough_stone.item.tool.*
 
-class ModItems {
-    companion object{
-        val GEOMATTER = Geomatter()
-        private val items: Array<Item> = arrayOf(
-            GEOMATTER,
-            GeomatterPickaxe(),
-            GeomatterAxe(),
-            GeomatterSword(),
-            GeomatterShovel(),
-            GeomatterHoe(),
-            GeomatterMegaPickaxe(),
-            GeomatterMegaShovel()
-        )
+object ModItems {
+    val GEOMATTER = Geomatter()
+    private val items: Array<Item> = arrayOf(
+        GEOMATTER,
+        GeomatterPickaxe(),
+        GeomatterAxe(),
+        GeomatterSword(),
+        GeomatterShovel(),
+        GeomatterHoe(),
+        GeomatterMegaPickaxe(),
+        GeomatterMegaShovel(),
+        GeomatterClaymore()
+    )
 
-        fun initialize() {
-            for (item: Item in items){
-                Registry.register(Registries.ITEM, (item as Identifiable).getId(), item)
-                if (item is Groupable)
-                    ItemGroupEvents.modifyEntriesEvent(item.getGroup()).register { content -> content.add(item) }
-            }
-
-            AreaMiner.registerBeforeBlockBreakListeners()
+    fun initialize() {
+        for (item: Item in items){
+            Registry.register(Registries.ITEM, (item as Identifiable).getId(), item)
+            if (item is Groupable)
+                ItemGroupEvents.modifyEntriesEvent(item.getGroup()).register { content -> content.add(item) }
         }
+
+        AreaMiner.registerBeforeBlockBreakListeners()
     }
 }
---
+++ src/main/kotlin/org/wdfeer/not_enough_stone/item/tool/GeomatterClaymore.kt
@@ -0,0 +1,46 @@
+package org.wdfeer.not_enough_stone.item.tool
+
+import com.google.common.collect.Multimap
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.attribute.EntityAttribute
+import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.item.ItemStack
+import net.minecraft.item.SwordItem
+import net.minecraft.text.Text
+import net.minecraft.world.World
+import org.wdfeer.not_enough_stone.item.Geomatter
+import org.wdfeer.not_enough_stone.item.Identifiable
+import org.wdfeer.not_enough_stone.material.GeomatterMaterial
+import org.wdfeer.not_enough_stone.tooltip.StonesCombinedTooltip
+
+class GeomatterClaymore : SwordItem(GeomatterMaterial.INSTANCE, 4, 1f - 4f, FabricItemSettings()), GeomatterWeapon,
+    Identifiable {
+    override fun getIdName(): String = "geomatter_claymore"
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        StonesCombinedTooltip.appendTooltip(stack, tooltip)
+        if (stack != null) {
+            tooltip?.add(getDamageIncreaseTooltip(getDamageIncrease(stack.orCreateNbt.getInt(Geomatter.STONES_COMBINED_NBT))))
+        }
+    }
+
+    override fun getAttributeModifiers(
+        stack: ItemStack?,
+        slot: EquipmentSlot?
+    ): Multimap<EntityAttribute, EntityAttributeModifier> {
+        if (stack == null || slot == null) return getAttributeModifiers(stack, slot)
+
+        return getAttributeModifiers(getIdName(), stack, slot, getAttributeModifiers(slot))
+    }
+
+    override fun getDamageIncrease(stones: Int): Float {
+        return super.getDamageIncrease(stones) * 1.6f
+    }
+}
